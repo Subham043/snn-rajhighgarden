60c8a36e8360b7d11e249e6b4b3903b3e9c1cdec
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-08-04 23:21:21 +0530

    done

--- src/app/Http/Services/OtpService.php
+++ src/app/Http/Services/OtpService.php
@@ -9,7 +9,7 @@ class OtpService
 
     public function sendOtp(Int $phone, Int $otp): void
     {
-        Http::get('https://sms.sendmsg.in/smpp?username='.env('SMS_USERNAME').'&password='.env('SMS_PASSWORD').'&from='.env('SMS_FROM').'&to=91'.$phone.'&text='.$otp.' is your One Time Password - OTP for inquiry on the website. Please do not share this with anyone. Thanks %26 regards, Team SNN Properties LLP.&urlshortening=1');
+        Http::get('https://sms.sendmsg.in/smpp?username='.config('app.sms_username').'&password='.config('app.sms_password').'&from='.config('app.sms_from').'&to=91'.$phone.'&text='.$otp.' is your One Time Password - OTP for inquiry on the website. Please do not share this with anyone. Thanks %26 regards, Team SNN Properties LLP.&urlshortening=1');
     }
 
 }
